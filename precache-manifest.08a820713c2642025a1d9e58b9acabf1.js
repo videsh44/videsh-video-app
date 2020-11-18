fc79c272adcffdc53c28056474a24e797127d8f8
videsh <videsh.ghodarop@iaugmentor.com>
2020-11-18 13:31:37 +0530

    Updates

---
+++ precache-manifest.08a820713c2642025a1d9e58b9acabf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf1111c5eda67b8eaa4ce75bcfc8e02",
+    "url": "/videsh-video-app/index.html"
+  },
+  {
+    "revision": "dd42ee46ad0c61b102e9",
+    "url": "/videsh-video-app/static/js/2.6f5dd003.chunk.js"
+  },
+  {
+    "revision": "482308fce8537f1b17141b6527a207c9",
+    "url": "/videsh-video-app/static/js/2.6f5dd003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a52a57284bf762e708",
+    "url": "/videsh-video-app/static/js/main.5bcea4b6.chunk.js"
+  },
+  {
+    "revision": "a9eed37f8771ce37829d",
+    "url": "/videsh-video-app/static/js/runtime-main.b2b96279.js"
+  },
+  {
+    "revision": "0e56efd0a097beb41d6d8543df276ac6",
+    "url": "/videsh-video-app/static/media/google-logo.0e56efd0.svg"
+  }
+]);
